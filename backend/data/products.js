df1e33cf4e42dcdeb7c34ad387160a8072fd1394
Oluwajoba <jobafash3@gmail.com>
2020-10-04 00:58:09 +0100

    Sections 1-6

---
+++ backend/data/products.js
@@ -0,0 +1,50 @@
+const products = [
+  {
+    name: "Gown",
+    image: "/images/IMG-20200813-WA0084.jpg",
+    description: "Short skimpy gown for that august event",
+    brand: "Belvins Apparels",
+    category: "Clothes",
+    price: 10000,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "See through gown",
+    image: "/images/IMG-20200913-WA0001.jpg",
+    description: "A classy lace gown with sparkling stones",
+    brand: "Belvins Apparels",
+    category: "Clothes",
+    price: 12000,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "Butterfly dress",
+    image: "/images/733634ff8f2bae488fbad9ded1100815.jpg",
+    description:
+      "Characterized by versatile designs, the BD further clarifies itself using a unique form",
+    brand: "Belvins Apparels",
+    category: "Clothes",
+    price: 11000,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: "Blue Owambe gown",
+    image: "/images/IMG-20200301-WA0007.jpg",
+    description:
+      "The ultimate fashion trend. Whether you are going to Owambe, or a trad-only, it is a perfect choicee",
+    brand: "Belvins Apparel",
+    category: "Clothes",
+    price: 14000,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+];
+
+export default products;
